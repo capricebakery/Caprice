01413359c009086f4c9977e82682dda1f38a9535
Melissa Ariza <melqwwq@gmail.com>
2025-03-01 19:47:51 -0500

    Actualizacion Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
-function App() {
+export default function App() {
   return (
-      <div>
-        <span>
-          Hola Mundo
-        </span> 
-      </div>
-  )
+    <div className="flex items-center justify-center h-screen bg-indigo-500">
+      <h1 className="text-3xl font-bold text-blue-500">
+        ¡Tailwind CSS está funcionando! 🚀
+      </h1>
+    </div>
+  );
 }
 
-export default App
