939e7de36a5612f6c5c3ff95ee69be2d3ade5fe5
Camila Perdomo <ednaquintero-4@hotmail.com>
2025-03-03 13:15:42 -0500

    Actualizacion Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-indigo-500">
-      <h1 className="text-3xl font-bold text-blue-500">
+      <h1 className="text-3xl font-bold text-white-500">
         ¡Tailwind CSS está funcionando! 🚀
       </h1>
     </div>
