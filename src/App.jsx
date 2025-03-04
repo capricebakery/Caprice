d2fd635906379ef4066ff0b656c435d3312a908e
Melissa Ariza <melqwwq@gmail.com>
2025-03-04 14:32:47 -0500

    Creacion Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-export default function App() {
+import Banner from "./components/Banner/Banner.jsx";
+export default function App()
+
+
+{
   return (
-    <div className="flex items-center justify-center h-screen bg-indigo-500">
-      <h1 className="text-3xl font-bold text-white-500">
-        ¡Tailwind CSS está funcionando! 🚀
-      </h1>
+    <div>
+      <Banner></Banner>
     </div>
   );
 }
