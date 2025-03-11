2f8d20cba49bfd1758e47156a6353a555e267caa
Melissa Ariza <melqwwq@gmail.com>
2025-03-11 15:02:57 -0500

    Creacion de las secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Banner from "./components/Banner/Banner.jsx";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Secciones from "./components/Secciones/Secciones.jsx";
+import Menu from "./components/Menu/Menu.jsx";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Header/>
       <main>
         <Banner/>
+        <Secciones/>
+        <Menu/>
       </main>
       <Footer/>
     </div>
